e35edf13ad42c4d3280ac509c60a3bf69c86b49b
Carlos Zamudio <a01799283@tec.mx>
2025-10-15 00:28:16 -0600

    add: spark

---
+++ ML/src/main.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+
+
+load_dotenv()
+
+
+def main():
+    spark_app_name = os.getenv("SPARK_APP_NAME")
+    spark_master_url = os.getenv("SPARK_MASTER_URL")
+    postgres_url = os.getenv("POSTGRES_URL")
+    postgres_db = os.getenv("POSTGRES_DB")
+    postgres_user = os.getenv("POSTGRES_USER")
+    postgres_password = os.getenv("POSTGRES_PASSWORD")
+
+    spark = (
+        SparkSession.builder.appName(spark_app_name)
+        .master(spark_master_url)
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.8")
+        .config("spark.ui.showConsoleProgress", "false")
+        .config("spark.sql.adaptive.enabled", "true")
+        .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
+        .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+        .config("spark.sql.execution.arrow.pyspark.fallback.enabled", "false")
+        .getOrCreate()
+    )
+
+    df = (
+        spark.read.format("jdbc")
+        .option("url", postgres_url)
+        .option("dbtable", postgres_db + ".articles")
+        .option("user", postgres_user)
+        .option("password", postgres_password)
+        .option("driver", "org.postgresql.Driver")
+        .load()
+    )
+
+    df.show()
+
+
+if __name__ == "__main__":
+    main()
