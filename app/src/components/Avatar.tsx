24a036a0748d8e1055f8125e78ae2156b66e0d59
A01750047 <117103901+A01750047@users.noreply.github.com>
2025-11-24 19:51:25 -0600

    Avatar integration

--- app/src/components/Avatar.tsx
+++ app/src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import "../assets/styles/avatar.css";
 
 type AvatarState = "standby" | "talking" | "thinking";
